d5b17a4e02eeb5111507c7cfbae1077ea510e5f0
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2022-08-10 16:45:36 -0300

    responsividade home

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css';
 import LogotipoRed from '../../assets/img/LogotipoRed.png'
+
+
+
+
 function Navbar() {
+    const [isActive, setActive] = useState("false");
+
+    const handleToggle = () => {
+        setActive(!isActive);
+    };
+    
     return (
         <div className='header'>
             <header>
                 <a href='#'><img className='logo' src={LogotipoRed} ></img></a>
-                <ul className='navigation'>
+                <div onClick={handleToggle}  className={isActive ? "toggle" : "toggle active"}></div>
+                <ul className={isActive ? "navigation" : "navigation active"}>
                     <li><a href='#'>Home</a></li>
                     <li><a href='#'>Test</a></li>
                     <li><a href='#'>Test</a></li>

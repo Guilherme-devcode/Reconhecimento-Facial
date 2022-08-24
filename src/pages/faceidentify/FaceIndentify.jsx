eef23d4b0b7302f3ed10363cf3b578315fa7a2c0
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2022-08-24 17:11:28 -0300

    alterando home e face-recognition

--- src/pages/faceidentify/FaceIndentify.jsx
+++ src/pages/faceidentify/FaceIndentify.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/heading-has-content */
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { useEffect, useRef, useState } from 'react';
 import * as faceapi from "face-api.js";
@@ -6,6 +9,7 @@ import face from '../../assets/img/face.png'
 import { PuffLoader } from 'react-spinners';
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { getDatabase, getDetectedFaces, setDataBase } from '../../firebase';
+import { fetchImage } from 'face-api.js';
 
 function FaceIndentify() {
   const videoRef = useRef()
@@ -13,162 +17,164 @@ function FaceIndentify() {
   const { Canvas, Image, ImageData } = canvas;
   const [loading, setLoading] = useState(true)
   let [isActive, setActive] = useState(true);
+
+
+
   const getVideo = async () => {
-    await navigator.mediaDevices
-      .getUserMedia({ video: { width: 300 } })
-      .then(stream => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(err => {
-        console.error("error:", err);
-      });
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { width: 300 } })
+    let video = videoRef.current;
+    video.srcObject = await stream;
+    await video.play();
+    await handleImage()
+    await setLoading(false)
+
   };
 
 
 
   const loadLabels = async () => {
-
     const result = await getDatabase("detectedPeople")
     const labels = result.map(m => m.name)
     return Promise.all(labels.map(async label => {
       const descriptions = []
-      const img = await canvas.loadImage(`https://raw.githubusercontent.com/Guilherme-devcode/Reconhecimento-Facial/main/public/labels/${label}/1.png`);
-      img.crossOrigin = "anonymous";
+      const img = await fetchImage(`https://raw.githubusercontent.com/Guilherme-devcode/Reconhecimento-Facial/main/public/labels/${label}/1.png`);
       const detections = await faceapi.detectSingleFace(img)
         .withFaceLandmarks()
         .withFaceDescriptor()
       descriptions.push(detections.descriptor)
       return new faceapi.LabeledFaceDescriptors(label, descriptions)
     }))
+
   }
 
 
   const handleImage = async () => {
-    await getVideo()
-    setTimeout(async () => {
-      const canvas = await faceapi.createCanvasFromMedia(videoRef.current)
-      const labels = await loadLabels()
-      // document.body.append(canvas)
-      const displaySize = { width: videoRef.current.width, height: videoRef.current.height }
-      faceapi.matchDimensions(canvas, displaySize)
-      setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi
-          .TinyFaceDetectorOptions())
-          .withFaceLandmarks()
-          .withFaceExpressions()
-          .withFaceDescriptors()
-          .withAgeAndGender()
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        // ------ Aumenta a precisão da captura do rosto com a imagem da pessoa em 90%
-        const faceMatcher = new faceapi.FaceMatcher(labels, 0.9)
-        const results = resizedDetections.map(d =>
-          faceMatcher.findBestMatch(d.descriptor)
-        )
-        //-----Desenhar linhas para detecção--------
-        // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        // faceapi.draw.drawDetections(canvas, resizedDetections)
-        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-        resizedDetections.forEach(detection => {
-          const { age, gender, genderProbality } = detection
-          //----------Detectar idade e genero da pessoa---------- 
-          new faceapi.draw.DrawTextField([
-            `${parseInt(age, 10)} years`,
-            `${gender} (${parseInt(genderProbality * 100, 10)})%`
-          ], detection.detection.box.topRight).draw(canvas)
+    let screen = document.getElementById("recognized-screen");
+    let name = document.getElementById("name");
+    screen.classList.remove("active")
+    name.classList.remove("active")
+    const canvas = await faceapi.createCanvasFromMedia(videoRef.current)
+    const labels = await loadLabels()
+    // document.body.append(canvas)    
+    const displaySize = { width: videoRef.current.width, height: videoRef.current.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi
+      .TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withFaceDescriptors()
+      .withAgeAndGender()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    // ------ Adicionando mais um paramentro com numero na função faceMatcher vai limitar a similaridade do rosto da webcam com a foto
+    const faceMatcher = new faceapi.FaceMatcher(labels)
+    const results = resizedDetections.map(d =>
+      faceMatcher.findBestMatch(d.descriptor)
+    )
+    setTypes(results)
+  }
 
-        })
-        results.forEach(async (result, index) => {
-          // ------- Desenha um quadrado azul no rosto e as descrições da pessoa
-          // const box = resizedDetections[index].detection.box
-          // const { label, distance } = result
-          // new faceapi.draw.DrawTextField([
-          //   `${label} (${distance})`
-          // ], box.bottomRight).draw(canvas)
-
-          // ------- Get do firebase com nome, tipo e id da pessoa 
-          const resultDb = await getDatabase("detectedPeople")
-          const labels = resultDb.find(label => label.name === result._label);
-          let dateLabel = labels.date
-          const fireBaseTime = new Date(
-            dateLabel.seconds * 1000 + dateLabel.nanoseconds / 1000000,
-          );
-          const date = fireBaseTime.toDateString();
-          const atTime = fireBaseTime.toLocaleTimeString();
-          var currentDate = new Date();
-          console.log(currentDate);
-          console.log(date, atTime);
-          if (labels?.type === 1) {
-            const name = `Seja bem vindo ${labels.name}`
+  const setTypes = async (results) => {
+    if (results.length === 0) {
+      const name = `Nenhum Rosto encontrado`
+      document.getElementById("unknown").innerHTML = name;
+      setTimeout(() => {
+        handleImage()
+      }, 1000);
+    }
+    results.forEach(async (result) => {
+      if (result._label === 'unknown') {
+        const name = `Não detectado`
+        document.getElementById("unknown").innerHTML = name;
+        handleImage()
+      }
+      setTimeout(async () => {
+        const resultDb = await getDatabase("detectedPeople")
+        const labels = resultDb.find(label => label.name === result?._label);
+        let typeLabel = labels?.type;
+        switch (typeLabel) {
+          case 1:
+            const name = `Seja bem vindo ${result?._label}`
             document.getElementById("name").innerHTML = name;
-          } else if (labels?.type === 2) {
-            const name = `Volte sempre ${labels.name}`
-            document.getElementById("name").innerHTML = name;
-          } else if (labels === undefined) {
-            const name = `Não detectado`
-            document.getElementById("name").innerHTML = name;
-          }
-        })
-      }, 1000)
-      setActive(isActive = false);
-    }, 3000);
+            let element = document.getElementById("name");
+            element.classList.add("active")
+            let screen = document.getElementById("recognized-screen");
+            screen.classList.add("active")
+            await setDataBase(labels.id, 2)
+            setTimeout(() => {
+              handleImage()
+            }, 10000);
+            break
+          case 2:
+            let element2 = document.getElementById("name");
+            element2.classList.add("active")
+            let screen2 = document.getElementById("recognized-screen");
+            screen2.classList.add("active")
+            const name2 = `Volte sempre ${result?._label}`
+            document.getElementById("name").innerHTML = name2;
+            await setDataBase(labels.id, 1)
+            setTimeout(() => {
+              handleImage()
+            }, 10000);
+            break
+          default:
+        }
+      })
+    }, 2000);
   }
 
 
 
 
+
+
+  const loadModels = async () => {
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+      faceapi.nets.ageGenderNet.loadFromUri("/models"),
+      faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+      getVideo()
+    ])
+  }
+
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false)
-    }, 1000);
-    const loadModels = () => {
-      Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-        faceapi.nets.ageGenderNet.loadFromUri("/models"),
-        faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-      ]).then(handleImage)
-        .catch((e) => console.log(e))
-    }
-    videoRef.current && loadModels()
+    loadModels()
   })
+
+
   return (
     <div className="container">
-      {
-        loading ?
-          <PuffLoader color={'#fff'} loading={loading} size={100} />
-          :
-          <div className='content-face-id'>
-            <div className='text-face-id'>
-              <h2>Identificação facial</h2>
-              <span>Posicione seu rosto na demarcação</span>
-            </div>
-
-            <div className='video-content-id'>
-              <img
-                crossOrigin='anonymous'
-                className={isActive ? "outline-face-id" : "outline-face-id active"}
-                src={face}>
-              </img>
-              <video
-                id='video'
-                crossOrigin='anonymous'
-                className='video'
-                width="640"
-                height="350"
-                autoPlay
-                ref={videoRef}
-                muted>
-              </video>
-              <h3 id="name" className="name"></h3>
-              {/* <h2 id="idade"></h2> */}
-            </div>
+      <PuffLoader className='loading' color={'#fff'} loading={loading} size={100} />
+      <div className={loading ? "loading-background" : "loading-background display-none"}></div>
+      <div className='content-face-id'>
+        <div className='text-face-id'>
+          <h2>Identificação facial</h2>
+          <span>Posicione seu rosto na demarcação</span>
+        </div>
+        <div className='video-content-id'>
+          <img
+            crossOrigin='anonymous'
+            className={isActive ? "outline-face-id" : "outline-face-id active"}
+            src={face}>
+          </img>
+          <video
+            id='video'
+            crossOrigin='anonymous'
+            className='video'
+            width="640"
+            height="350"
+            autoPlay
+            ref={videoRef}
+            muted>
+          </video>
+          <h3 id="unknown" className="unknown"></h3>
+          <div id='recognized-screen' className='recognized-screen'>
+            <h3 id="name" className="name"></h3>
           </div>
-      }
+        </div>
+      </div>
     </div >
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       <div className='text'>
         <h2>Seja Bem vindo!</h2>
         <p>Adipisicing ullamco amet sit laboris ex esse quis. Cupidatat exercitation consectetur est eiusmod. Id mollit voluptate officia officia eiusmod ipsum occaecat non. Enim reprehenderit amet reprehenderit fugiat pariatur sit id occaecat duis proident fugiat sint.</p>
-        <Link className='btn' to="/check-in">Faça o check-in</Link>
+        <Link className='btn' to="/check-in">Reconhecimento</Link>
       </div>
       <img className='face-id' src={faceId}></img>
     </div >
